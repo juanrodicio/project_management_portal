f923804bf2a06f8a4028b8cac8ad79e386bdc5ad
juanrodicio <juanantonio.rodiciolopez@alum.uca.es>
2018-01-08 20:41:05 +0100

    Modified tables

--- project_management_portal_tables.sql
+++ project_management_portal_tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 07-01-2018 a las 19:05:35
+-- Tiempo de generación: 08-01-2018 a las 19:39:56
 -- Versión del servidor: 5.7.19
 -- Versión de PHP: 7.1.9
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `project_management_portal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `issue_commentaries`
+--
+
+DROP TABLE IF EXISTS `issue_commentaries`;
+CREATE TABLE IF NOT EXISTS `issue_commentaries` (
+  `Issue_ID` int(11) NOT NULL,
+  `Commentary_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Commentary_Title` tinytext NOT NULL,
+  `Commentary_Comment` text NOT NULL,
+  `Commentary_Author` varchar(30) NOT NULL,
+  PRIMARY KEY (`Commentary_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `issue_commentaries`
+--
+
+INSERT INTO `issue_commentaries` (`Issue_ID`, `Commentary_ID`, `Commentary_Title`, `Commentary_Comment`, `Commentary_Author`) VALUES
+(1, 1, 'Title for commentary 1', 'Here\'s just a commentary to help me on see how it will we save! ', 'juanrodicio'),
+(1, 2, 'yeah you sure!', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', 'juanrodicio'),
+(1, 4, 'About logins', 'I\'ll improve our login in that way, best option is to use a new Framework that let us seed all our users.\r\n\r\nCheck out this: http://www.yiiframework.com/', 'luisrozo');
 
 -- --------------------------------------------------------
 
@@ -88,6 +113,7 @@ CREATE TABLE IF NOT EXISTS `project_members` (
 
 INSERT INTO `project_members` (`Project_ID`, `User_Name`) VALUES
 (1, 'juanrodicio'),
+(1, 'luisrozo'),
 (2, 'juanrodicio');
 
 -- --------------------------------------------------------
@@ -107,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `project_task` (
   `Task_Project` int(11) NOT NULL,
   PRIMARY KEY (`Task_ID`),
   KEY `Task_Project` (`Task_Project`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `project_task`
@@ -115,7 +141,36 @@ CREATE TABLE IF NOT EXISTS `project_task` (
 
 INSERT INTO `project_task` (`Task_ID`, `Task_Name`, `Task_Description`, `Task_StartDate`, `Task_FinishDate`, `Task_Status`, `Task_Project`) VALUES
 (1, 'Task 1', 'Just a description task', '2018-01-03', '2018-01-06', 'Done', 1),
-(2, 'Task 2', 'Just a description 2', '2018-01-11', '2018-01-13', 'Done', 1);
+(2, 'Task 2', 'Just a description 2', '2018-01-11', '2018-01-13', 'Done', 1),
+(3, 'Task 3', 'Description task 3', '2018-01-07', '2018-01-10', 'Done', 1),
+(4, 'Task 4', 'Description of task 4', '2018-01-08', '2018-01-10', 'Pending', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `project_task_issues`
+--
+
+DROP TABLE IF EXISTS `project_task_issues`;
+CREATE TABLE IF NOT EXISTS `project_task_issues` (
+  `Task_ID` int(11) NOT NULL,
+  `Issue_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Issue_Title` tinytext NOT NULL,
+  `Issue_Description` text NOT NULL,
+  `Issue_Type` enum('info','warning','danger','') NOT NULL,
+  `Issue_Status` enum('active','closed') NOT NULL,
+  `Issue_Author` varchar(30) NOT NULL,
+  PRIMARY KEY (`Issue_ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `project_task_issues`
+--
+
+INSERT INTO `project_task_issues` (`Task_ID`, `Issue_ID`, `Issue_Title`, `Issue_Description`, `Issue_Type`, `Issue_Status`, `Issue_Author`) VALUES
+(1, 1, 'Problem with consecutive logins on website', 'You\'ll see here what is the issue about. Let\'s see how long can we got here and see how about to see it on webpage :)', 'info', 'active', 'juanrodicio'),
+(1, 8, 'Issue to close', 'Just need to close it.', 'danger', 'closed', 'luisrozo'),
+(1, 3, 'Cascade Classifier needs improvement', 'Actually classifier continue to be so low instead of LBP, we may need to change to this one or make a better code of actual classifier.', 'warning', 'active', 'juanrodicio');
 
 -- --------------------------------------------------------
 
@@ -141,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`User_Name`, `User_Password`, `User_Type`, `User_Group`, `User_FullName`, `User_Email`) VALUES
 ('luisrozo', 'luisrozo', 'Stakeholder', '1', 'Luis Gonzaga Rozo Bueno', 'luis.rozo@alum.uca.es'),
-('juanrodicio', 'defensa5', 'Pig', '1', 'Juan Antonio Rodicio', 'juanantonio.rodiciolopez@alum.uca.es');
+('juanrodicio', 'defensa10', 'Pig', '1', 'Juan Antonio Rodicio López', 'juanantonio.rodiciolopez@alum.uca.es');
 
 -- --------------------------------------------------------
 
@@ -155,6 +210,17 @@ CREATE TABLE IF NOT EXISTS `user_tasks` (
   `User_Name` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`Task_ID`,`User_Name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `user_tasks`
+--
+
+INSERT INTO `user_tasks` (`Task_ID`, `User_Name`) VALUES
+(1, 'juanrodicio'),
+(1, 'luisrozo'),
+(2, 'luisrozo'),
+(3, 'juanrodicio'),
+(4, 'luisrozo');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
